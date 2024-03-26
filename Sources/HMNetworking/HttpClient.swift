d4d641ff63ffcbba3ada6a774d75aa0a6c6bb9fc
Arthur <archiworking.d@gmail.com>
2024-03-26 19:36:57 +0300

    File uploading

--- Sources/HMNetworking/HttpClient.swift
+++ Sources/HMNetworking/HttpClient.swift
@@ -9,35 +9,37 @@ import Foundation
 import Alamofire
 
 public struct HttpClient {
+    var session: Session
     var defaultRequest = DefaultRequest()
     var defaultPlugins: [PluginType] = []
     
     public init(
+        session: Session = AF,
         @HttpPluginsBuilder plugins: () -> [PluginType] = { [] },
         @HttpDefaultRequestBuilder preferences: () -> [HttpDefaultRequestPreference]
     ) {
-        defaultRequest = defaultRequest.apply(preferences: preferences())
-        defaultPlugins = plugins()
+        self.session = session
+        self.defaultRequest = defaultRequest.apply(preferences: preferences())
+        self.defaultPlugins = plugins()
     }
     
     public init(
+        session: Session = AF,
         @HttpPluginsBuilder plugins: () -> [PluginType] = { [] }
     ) {
-        self.init(plugins: plugins, preferences: { })
+        self.init(session: session, plugins: plugins, preferences: { })
     }
     
     // MARK: - Basic requests
     public func request(_ convertible: URLConvertible,
-                 session: Session = AF,
-                 @HttpPluginsBuilder plugins: () -> [PluginType] = { [] }
+                        @HttpPluginsBuilder plugins: () -> [PluginType] = { [] }
     ) async throws -> AFDataResponse<Data?> {
-        try await request(convertible, session: session, plugins: plugins, preferences: { })
+        try await request(convertible, plugins: plugins, preferences: { })
     }
     
     public func request(_ convertible: URLConvertible,
-                 session: Session = AF,
-                 @HttpPluginsBuilder plugins: () -> [PluginType] = { [] },
-                 @HttpRequestBuilder preferences: () -> [HttpRequestPreference]
+                        @HttpPluginsBuilder plugins: () -> [PluginType] = { [] },
+                        @HttpRequestBuilder preferences: () -> [HttpRequestPreference]
     ) async throws -> AFDataResponse<Data?> {
         
         let url = defaultRequest.host + convertible
@@ -45,7 +47,36 @@ public struct HttpClient {
             .apply(preferences: preferences())
         
         let handler = session.request(request)
+        let response = await withUnsafeContinuation { continuation in
+            handler
+                .apply(plugins: defaultPlugins + plugins())
+                .response { response in
+                    continuation.resume(returning: response)
+                }
+        }
         
+        return try defaultRequest.responseHandler(response)
+    }
+    
+    // MARK: - Download requests
+    
+    public func upload(_ convertible: URLConvertible,
+                       @HttpPluginsBuilder plugins: () -> [PluginType] = { [] }
+    ) async throws -> AFDataResponse<Data?> {
+        try await upload(convertible, plugins: plugins, preferences: { })
+    }
+    
+    public func upload(_ convertible: URLConvertible,
+                       @HttpPluginsBuilder plugins: () -> [PluginType] = { [] },
+                       @HttpRequestBuilder preferences: () -> [HttpRequestPreference]
+    ) async throws -> AFDataResponse<Data?> {
+        
+        let url = defaultRequest.host + convertible
+        let request = try URLRequest(url: url, method: .get, headers: defaultRequest.headers)
+            .apply(preferences: preferences())
+        
+        let data = request.httpBody ?? Data()
+        let handler = session.upload(data, with: request)
         let response = await withUnsafeContinuation { continuation in
             handler
                 .apply(plugins: defaultPlugins + plugins())
--- Sources/HMNetworking/Types/PluginType.swift
+++ Sources/HMNetworking/Types/PluginType.swift
@@ -15,14 +15,3 @@ public protocol PluginType {
 public extension PluginType {
     func prepare(request: Request) -> Request { request }
 }
-
-extension DataRequest {
-    func apply(plugins: [any PluginType]) -> DataRequest {
-        var request: DataRequest = self
-        plugins.forEach { plugin in
-            request = plugin.prepare(request: request)
-        }
-        
-        return request
-    }
-}
