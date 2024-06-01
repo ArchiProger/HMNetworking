94047b62071cdf32c49258959f45233c35c849c8
Archibbald <88779697+ArchiProger@users.noreply.github.com>
2024-06-01 12:59:28 +0300

    Authentication builder

---
+++ Sources/HMNetworking/Features/Auth/Entities/Authentication.swift
@@ -0,0 +1,52 @@
+//
+//  Authentication.swift
+//  HMNetworking
+//
+//  Created by Archibbald on 31.05.2024.
+//
+
+import Foundation
+
+public struct Authentication: HttpClientConfig {
+    var perform: (HttpRequest) -> HttpRequest
+    
+    public init(credential: URLCredential) {
+        perform = {
+            var result = $0
+            result.credential = credential
+            
+            return result
+        }
+    }
+    
+    public init(_ value: String) {
+        perform = {
+            var result = $0
+            result.headers.add(.authorization(value))
+            
+            return result
+        }
+    }
+    
+    public init(bearer token: String) {
+        perform = {
+            var result = $0
+            result.headers.add(.authorization(bearerToken: token))
+            
+            return result
+        }
+    }
+    
+    public init(username: String, password: String) {
+        perform = {
+            var result = $0
+            result.headers.add(.authorization(username: username, password: password))
+            
+            return result
+        }
+    }
+    
+    public func prepare(request: HttpRequest) -> HttpRequest {
+        perform(request)
+    }
+}
